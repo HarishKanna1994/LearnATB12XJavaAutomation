7cb97614cc3390bb2e938e20213a9d8deeda8bcc
HarishKanna <hhharish.kanna@gmail.com>
2025-05-13 19:22:11 +0530

    Commit1

---
+++ src/ex01_Java_Basics/Lab003_Comments.java
@@ -0,0 +1,16 @@
+package ex01_Java_Basics;
+
+public class Lab003_Comments {
+    public static void main(String[] args) {
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        //This is a comment
+        System.out.println("HelloWorld");
+    }
+}
