611d049597ce11e4882f388541c69c784d2964ec
HarishKanna <hhharish.kanna@gmail.com>
2025-05-13 20:33:23 +0530

    Commit1

---
+++ src/ex01_Java_Basics/Lab008_println_vs_print.java
@@ -0,0 +1,9 @@
+package ex01_Java_Basics;
+
+public class Lab008_println_vs_print {
+    public static void main(String[] args) {
+        System.out.print("Hi");
+        System.out.println("Bye");
+        System.out.print("Hello");
+    }
+}
