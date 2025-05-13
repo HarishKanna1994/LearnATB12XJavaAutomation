ca68428146bf0785adcb73cfb1ad39ee508bf09b
HarishKanna <hhharish.kanna@gmail.com>
2025-05-13 19:42:43 +0530

    Commit1

---
+++ src/ex01_Java_Basics/Lab004_MultipleComments.java
@@ -0,0 +1,9 @@
+package ex01_Java_Basics;
+
+public class Lab004_MultipleComments {
+    /*
+    Author: HarishKanna
+    Course: Java
+
+     */
+}
---
+++ src/ex01_Java_Basics/Lab005_Comments_Shortcuts.java
@@ -0,0 +1,15 @@
+package ex01_Java_Basics;
+
+public class Lab005_Comments_Shortcuts {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+}
