98146843bc4dd8f34c9c404819f19e56413f3d37
HarishKanna <hhharish.kanna@gmail.com>
2025-05-17 02:38:19 +0530

    Literals

---
+++ src/ex03_Java_Literals/Lab022_Char_Literal.java
@@ -0,0 +1,42 @@
+package ex03_Java_Literals;
+
+public class Lab022_Char_Literal {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = 'B';
+        char c3 = '!';
+        char c4 = '@';
+        char c5 = '#';
+        char c6 = '^';
+        char c7 = '_';
+        char c8 = '+';
+        char c9 = ' ';
+
+        //Escape char
+        char newline = '\n';
+        char tab = '\t';
+        char backspace = '\b';
+        char carriagereturn = '\r';
+
+       /* System.out.println("HarishKanna");
+        System.out.println("Harish\nKanna");
+        System.out.println("Harish\tKanna");
+        System.out.println("Harish\bKanna");
+        System.out.println("Harish"+newline+"Kanna");
+        System.out.println("Harish"+tab+"Kanna");
+        System.out.println("Harish"+backspace+"Kanna");*/
+
+        char c11 = 'A';
+        System.out.println(c11);
+
+        char rupees = '₹';
+        System.out.println(rupees);
+
+        char laughsmiley = '\u1f60';
+        System.out.println(laughsmiley);
+
+
+
+
+    }
+}
---
+++ src/ex03_Java_Literals/Lab023_Nonprimitive_Literal.java
@@ -0,0 +1,22 @@
+package ex03_Java_Literals;
+
+public class Lab023_Nonprimitive_Literal {
+    public static void main(String[] args) {
+        //Primitive Data Type -> Defined by Java Guys
+        //Max, Min size is defined
+        //eg: int, float, boolean, short, byte
+
+        byte b=10;
+        System.out.println(b);
+        int a = 65;
+
+        //Non Primitive Data Type -> Defined by Users, Reference Data Types
+        //No size max,min
+        //Byte - 8 bits - 64
+
+        String name = "HarishKanna";  //String is a bunch of characters
+        int[] array_of_items = new int[10];
+
+
+    }
+}
